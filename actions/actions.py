25dd64827730dc80d2429137c81e5df5ea4fee95
git <borna.gajic2@gmail.com>
2021-08-04 17:16:59 +0200

    course_info

--- actions/actions.py
+++ actions/actions.py
@@ -7,6 +7,7 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
+from logging import info
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
@@ -37,7 +38,6 @@ class ActionMoreInfoProgramme(Action):
 
         with open('./knowledge_base_data/study_programme_info.json') as json_file:
             data = json.load(json_file)
-            response = ""
 
             try:
                 for (key, info) in data[degree][programme].items():        
@@ -72,20 +72,62 @@ class ActionCourseList(Action):
 
         with open('./knowledge_base_data/course_list.json') as json_file:
             data = json.load(json_file)
-            response = ""
 
             try:
-                for (key, courses) in data[degree][programme].items():
-                    
-                    k = key.replace('#', ' ')
-                    dispatcher.utter_message(text=f"{k}\n")
+                if programme == "":
+                    dispatcher.utter_message(text="Please specify a programme that you are interested in.")
+                elif degree == "":
+                    for (degrees, content) in data.items():
+                        
+                        if programme in content:
+                            for (key, courses) in content.items():
+                                k = key.replace('#', ' ')
+                                dispatcher.utter_message(text=f"{k}\n")
 
-                    for (key, name) in courses.items():
-                        dispatcher.utter_message(text=f"{key}, {name}\n")
-            except:
-                if len(programme) > 0 or len(degree) > 0:
-                    dispatcher.utter_message(text="Sorry, I don't understand your request. Please specify degree and programme that you are interested in.")
+                                for (id, name) in courses.items():
+                                    dispatcher.utter_message(text=f"{id}, {name}\n")
+                            break
                 else:
-                    dispatcher.utter_message(text="Sorry, I don't understand your request.")
+                    for (key, courses) in data[degree][programme].items():
+                        k = key.replace('#', ' ')
+                        dispatcher.utter_message(text=f"{k}\n")
+
+                        for (id, name) in courses.items():
+                            dispatcher.utter_message(text=f"{id}, {name}\n")
+            except:
+                dispatcher.utter_message(text="Sorry, I don't understand your request.")
+
+        return []
+
+class ActionCourseInfo(Action):
+
+    def name(self) -> Text:
+        return "action_course_info"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        course_id = ""
+        course_name = ""
+
+        for e in tracker.latest_message['entities']:
+            if e['entity'] == 'course_id':
+                course_id = e['value']
+            elif e['entity'] == 'course_name':
+                course_name = e['value']
+        
+        print("course info")
+        print(f"id {course_id}, name {course_name}")
+
+        with open('./knowledge_base_data/course_info.json') as json_file:
+            data = json.load(json_file)
+
+            try:
+                for (key, course_info) in data.items():
+                    if course_id.lower() in str(key).lower() and course_name.lower() in str(key).lower():
+                        dispatcher.utter_message(text=course_info)
+            except:
+                dispatcher.utter_message(text="Sorry, I don't understand your request.")
 
         return []
